9d652e080abdc3f3019d30406dfa03e619eca13e
Fellipe71 <Fjubinimendes@gmail.com>
2024-11-22 12:15:10 -0300

    changed logo and added event in JavaScript
    changed logo and added event in JavaScript

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const texto = document.getElementById('meuTexto');
 
 
 
-const precoPorQuilo = 5; // Preço em dólares por quilo de café
+const precoPorSaca = 100; // Preço em dólares por saca de café
 
 document.getElementById('converter').onclick = function() {
-    const kg = parseFloat(document.getElementById('kg').value);
-    if (!isNaN(kg) && kg >= 0) {
-        const totalDolares = kg * precoPorQuilo;
+    const sacas = parseFloat(document.getElementById('sacas').value);
+    if (!isNaN(sacas) && sacas >= 0) {
+        const totalDolares = sacas * precoPorSaca;
         document.getElementById('resultado').innerText = `Valor em dólares: $${totalDolares.toFixed(2)}`;
     } else {
         document.getElementById('resultado').innerText = 'Por favor, insira um valor válido.';
