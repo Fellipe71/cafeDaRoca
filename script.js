0570e38f17ef30ab645c016e79113cbba790a9e4
Fellipe71 <Fjubinimendes@gmail.com>
2024-12-02 12:04:38 -0300

    Updated last website files

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const texto = document.getElementById('meuTexto');
 
 
 
-const precoPorSaca = 100; // Preço em dólares por saca de café
 
 document.getElementById('converter').onclick = function() {
-    const sacas = parseFloat(document.getElementById('sacas').value);
+    let precoPorSaca = parseFloat(document.getElementById('cotacao').value);
+    let sacas = parseFloat(document.getElementById('sacas').value);
     if (!isNaN(sacas) && sacas >= 0) {
-        const totalDolares = sacas * precoPorSaca;
-        document.getElementById('resultado').innerText = `Valor em dólares: $${totalDolares.toFixed(2)}`;
+        let totalReais = sacas * precoPorSaca;
+        console.log(totalReais);
+        
+        document.getElementById('resultado').innerText = `Valor em Reais: R$${totalReais.toFixed(2)}`;
     } else {
         document.getElementById('resultado').innerText = 'Por favor, insira um valor válido.';
     }
